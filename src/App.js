16cf41bcaf5972b766b09ea91bf5c98b05b831be
Saykot43 <saykot.cse43@gmail.com>
2022-05-10 21:16:47 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import Appointment from './Appointment/Appointment';
+import Review from './Review/Review';
+import Contact from './Contact/Contact';
+import Login from './Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/review' element={<Review />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
